0ef0ccef2c9f64f1ecf6698cd18e5e12511654ed
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-26 16:51:10 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ import LoanItemsContextProvider from "@/contexts/LoanItemsContextProvider";
 
 export default function Home() {
   return (
-    <main className="bg-white h-screen w-screen flex md:flex-row items-center justify-center">
+    <main className="bg-white h-screen w-screen flex md:flex-row items-center justify-center p-4">
       <LoanItemsContextProvider>
-        <div className="flex flex-col  md:flex-row bg-blue-light h-full md:h-3/4 w-full md:w-3/4 shadow-2xl md:p-20 mx-auto ">
-          <div className=" bg-white text-black w-full md:w-1/2 h-full rounded-tl-3xl rounded-bl-3xl shadow-2xl p-6">
+        <div className="flex flex-col 2xl:items-center md:flex-row bg-blue-light h-full md:4/5 w-full md:w-4/5 shadow-2xl md:p-10 ">
+          <div className="flex flex-col 2xl:justify-center bg-white text-black w-full md:w-1/2 h-full 2xl:h-3/4 rounded-tl-3xl rounded-bl-3xl shadow-2xl p-6 ">
             <H1 />
             <MortgageAmount />
             <MortgageDetails />
@@ -21,7 +21,7 @@ export default function Home() {
               <CalcRepayments />
             </div>
           </div>
-          <div className=" bg-blue w-full md:w-1/2 h-full rounded-br-3xl rounded-tr-3xl shadow-2xl p-6">
+          <div className="flex flex-col 2xl:justify-center bg-blue w-full md:w-1/2 h-full 2xl:h-3/4 rounded-br-3xl rounded-tr-3xl shadow-2xl p-6">
             <HeaderDisplay />
             <DisplayPayments />
           </div>
